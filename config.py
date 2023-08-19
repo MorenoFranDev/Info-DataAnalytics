68429c1fda4b9b9afe2d9aad412912bd0290abbd
Francisco Dev <60716495+MorenoFranDev@users.noreply.github.com>
2023-08-20 01:57:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 cityList = ["London", "New York", "Cordoba", "Taipei", "Buenos Aires", ]
 coordList = ["lat=31&lon=64", "lat=40&lon=-73","lat=-31&lon=-64", "lat=25&lon=64", "lat=-34&lon=-58"]
 
-API_TOKEN = "5eaa008788ab7b6052f7e0680c86eb7a"
+API_TOKEN = ""
 BASE_URL = "https://api.openweathermap.org/data/2.5/onecall/timemachine?"
 DATABASE_URL = "postgresql://postgres:root@localhost:5432/informatorio"
 PATH = ".\Reports"
